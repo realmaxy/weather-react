8eb78edecb586b6674faa7f1fc6d9d4a94cec03f
realmaxy <max_sem777@mail.ru>
2023-03-21 18:09:28 +0300

    design fix

--- src/components/HourForecast/HourForecast.jsx
+++ src/components/HourForecast/HourForecast.jsx
@@ -12,7 +12,7 @@ export default function HourForecast() {
   const secondBlock = [dayList[2], dayList[3]]
 
   return (
-   <div style={glassStyle} className='max-w-max m-auto mt-3 mb-3 flex sm:flex-row flex-col'>
+   <div style={glassStyle} className='sm:w-max w-72 m-auto mt-3 mb-3 flex sm:flex-row flex-col'>
       <div className='flex'>{firstBlock.map((item, index) => <HourForecastItem key={index} index={index}title={item.content} forecast={today?.[index]}/>)}</div>
       <div className='flex'>{secondBlock.map((item, index) => <HourForecastItem key={index} index={index}title={item.content} forecast={today?.[index]}/>)}</div>
    </div>
